2961c4500a022c8a59cd8e41c680abe8dfbf3309
New-Noxs <matheo868@gmail.com>
2024-08-25 22:43:12 -0400

    Proyecto Completo

---
+++ js/app.js
@@ -0,0 +1,59 @@
+let claves = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat'
+};
+
+
+function copiar() {
+    let output = document.getElementById("output-text");
+    output.select();
+    document.execCommand("copy");
+}
+
+
+function desencriptar() {
+    let texto = document.getElementById("input-text").value;
+    let textoDesencriptado = texto
+        .replace(/ai/g, 'a')
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    let output = document.getElementById("output-text");
+    output.style.visibility = 'visible';
+    output.style.height = '70vh';
+    let waitingImage = document.getElementById("waiting-image");
+    waitingImage.hidden = true;
+
+    let titleMessage = document.getElementById("message-title");
+    titleMessage.hidden = true;
+
+    let mainMessage = document.getElementById("main-message");
+    mainMessage.hidden = true;
+    let copyButton = document.getElementById("copy-button");
+    
+    output.value = textoDesencriptado;
+    copyButton.hidden = false;
+}
+
+function encriptar() {
+    let texto = document.getElementById("input-text").value;
+    let textoEncriptado = texto
+        .replace(/[aeiou]/g, (match) => claves[match])
+        .replace(/[^a-z]/g, '');
+    let output = document.getElementById("output-text");
+    output.style.visibility = 'visible';
+    let waitingImage = document.getElementById("waiting-image");
+    waitingImage.hidden = true;
+    let titleMessage = document.getElementById("message-title");
+    titleMessage.hidden = true;
+    let mainMessage = document.getElementById("main-message");
+    mainMessage.hidden = true;
+    let copyButton = document.getElementById("copy-button");
+    copyButton.hidden = false;
+    output.style.height = '70vh';
+    output.value = textoEncriptado;
+}
